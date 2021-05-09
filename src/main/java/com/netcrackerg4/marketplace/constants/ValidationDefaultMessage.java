162d0bac2c8bccee73de6cf2c00f869bc40e5d60
Fillip Molodtsov <greatdrummer1@gmail.com>
2021-05-09 18:21:53 +0300

    adding error body response if request body doesn't pass validation

---
+++ src/main/java/com/netcrackerg4/marketplace/constants/ValidationDefaultMessage.java
@@ -0,0 +1,11 @@
+package com.netcrackerg4.marketplace.constants;
+
+public class ValidationDefaultMessage {
+    public static final String WRONG_FORMAT_EMAIL = "Wrong email format";
+    public static final String WRONG_FORMAT_PASSWORD =
+            "Password has to contain at least one capital letter, lower case letter and number";
+    public static final String WRONG_FORMAT_LAST_NAME =
+            "The first name must be of size between 2 and 30";
+    public static final String WRONG_FORMAT_FIRST_NAME=
+            "The last name must be of size between 2 and 30";
+}
---
+++ src/main/java/com/netcrackerg4/marketplace/model/domain/ErrorListBody.java
@@ -0,0 +1,12 @@
+package com.netcrackerg4.marketplace.model.domain;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import lombok.Builder;
+
+import java.util.Map;
+
+@Builder
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class ErrorListBody {
+    Map<String,String> error;
+}
