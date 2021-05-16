3c7441442dbf35ec9eaabcd6a069f9f3ad0411a5
Fillip Molodtsov <greatdrummer1@gmail.com>
2021-05-16 20:24:04 +0300

    change phoneNumber regexp

--- src/main/java/com/netcrackerg4/marketplace/constants/ValidationConstants.java
+++ src/main/java/com/netcrackerg4/marketplace/constants/ValidationConstants.java
@@ -4,5 +4,5 @@ public class ValidationConstants {
     public static final String NAME_PATTERN = "\\p{L}{2,30}";
     public static final String PASSWORD_PATTERN =
             "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$";
-    public static final String PHONE_PATTERN = "\\d{12,16}";
+    public static final String PHONE_PATTERN = "^\\+?[0-9]{3}-?[0-9]{6,12}$";
 }
