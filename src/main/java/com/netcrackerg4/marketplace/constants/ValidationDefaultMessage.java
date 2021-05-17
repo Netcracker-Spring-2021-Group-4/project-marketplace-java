ad989884ffdf9931d68ca6cb16780bd1507dd9e3
Ivan Mordas <ivanmordas49@gmail.com>
2021-05-17 14:39:41 +0300

    check phone

--- src/main/java/com/netcrackerg4/marketplace/constants/ValidationDefaultMessage.java
+++ src/main/java/com/netcrackerg4/marketplace/constants/ValidationDefaultMessage.java
@@ -8,4 +8,6 @@ public class ValidationDefaultMessage {
             "The first name must be of size between 2 and 30";
     public static final String WRONG_FORMAT_FIRST_NAME=
             "The last name must be of size between 2 and 30";
+    public static final String WRONG_FORMAT_PHONE_FORMAT=
+            "Phone has to contain ten digits and nothing else";
 }
