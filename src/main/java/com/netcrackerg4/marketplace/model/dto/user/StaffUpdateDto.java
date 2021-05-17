7ee573ecda0b26396a6557936b0e50d1e79411c2
Ivan Mordas <ivanmordas49@gmail.com>
2021-05-17 14:36:28 +0300

    create StaffUpdateDto

---
+++ src/main/java/com/netcrackerg4/marketplace/model/dto/user/StaffUpdateDto.java
@@ -0,0 +1,33 @@
+package com.netcrackerg4.marketplace.model.dto.user;
+
+
+import com.netcrackerg4.marketplace.constants.ValidationConstants;
+import com.netcrackerg4.marketplace.constants.ValidationDefaultMessage;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+
+@Data
+public class StaffUpdateDto {
+    @Email(message= ValidationDefaultMessage.WRONG_FORMAT_EMAIL)
+    private String email;
+
+    @Pattern(
+            regexp = ValidationConstants.NAME_PATTERN,
+            message = ValidationDefaultMessage.WRONG_FORMAT_FIRST_NAME
+    )
+    private String firstName;
+    @Pattern(
+            regexp = ValidationConstants.NAME_PATTERN,
+            message = ValidationDefaultMessage.WRONG_FORMAT_LAST_NAME
+    )
+    private String lastName;
+
+    @Pattern(
+            regexp = ValidationConstants.PHONE_PATTERN,
+            message = ValidationDefaultMessage.WRONG_FORMAT_PHONE_FORMAT
+    )
+    private String phone_number;
+
+}
