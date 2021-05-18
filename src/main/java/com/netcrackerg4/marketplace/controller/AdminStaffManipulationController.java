91f96cf2890cd6d0cfc3ea308d3f9914b09e5960
Ivan Mordas <ivanmordas49@gmail.com>
2021-05-18 11:58:03 +0300

    create staff controller

---
+++ src/main/java/com/netcrackerg4/marketplace/controller/AdminStaffManipulationController.java
@@ -0,0 +1,16 @@
+package com.netcrackerg4.marketplace.controller;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/admin/staff")
+public class AdminStaffManipulationController {
+
+    @PutMapping("/edit")
+        public void updateStaff(@Valid @RequestBody StaffUpdateDto staffUpdateDto){
+        staffService.updateUser(staffUpdateDto);
+    }
+}
