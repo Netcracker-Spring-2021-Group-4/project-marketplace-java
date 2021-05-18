26129dd092ad5605cf2d06e8bd32c1b83472ff64
Ivan Mordas <ivanmordas49@gmail.com>
2021-05-18 15:39:59 +0300

    add function

--- src/main/java/com/netcrackerg4/marketplace/controller/AdminStaffManipulationController.java
+++ src/main/java/com/netcrackerg4/marketplace/controller/AdminStaffManipulationController.java
@@ -1,4 +1,5 @@
 package com.netcrackerg4.marketplace.controller;
+import com.netcrackerg4.marketplace.model.dto.user.StaffUpdateDto;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
