a826817c285c64a2a30f85a753149b2795847fed
Ivan Mordas <ivanmordas49@gmail.com>
2021-05-18 15:52:15 +0300

    add field with sataffService

--- src/main/java/com/netcrackerg4/marketplace/controller/AdminStaffManipulationController.java
+++ src/main/java/com/netcrackerg4/marketplace/controller/AdminStaffManipulationController.java
@@ -1,5 +1,7 @@
 package com.netcrackerg4.marketplace.controller;
 import com.netcrackerg4.marketplace.model.dto.user.StaffUpdateDto;
+import com.netcrackerg4.marketplace.service.interfaces.IStaffService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +10,9 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/admin/staff")
+@RequiredArgsConstructor
 public class AdminStaffManipulationController {
+    private final IStaffService staffService;
 
     @PutMapping("/edit")
         public void updateStaff(@Valid @RequestBody StaffUpdateDto staffUpdateDto){
