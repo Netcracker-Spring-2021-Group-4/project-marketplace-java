66d04304d355ec54ee5d58c2e15baefa50460539
Ivan Mordas <ivanmordas49@gmail.com>
2021-05-21 10:44:37 +0300

    update function

--- src/main/java/com/netcrackerg4/marketplace/controller/StaffController.java
+++ src/main/java/com/netcrackerg4/marketplace/controller/StaffController.java
@@ -1,5 +1,6 @@
 package com.netcrackerg4.marketplace.controller;
 
+import com.netcrackerg4.marketplace.model.dto.user.ChangeStatusDto;
 import com.netcrackerg4.marketplace.model.dto.user.UserUpdateDto;
 import com.netcrackerg4.marketplace.service.interfaces.IUserService;
 import lombok.RequiredArgsConstructor;
