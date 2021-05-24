7438bd021a17c9e3e61c903bc42a277ff488a83c
Ivan Mordas <ivanmordas49@gmail.com>
2021-05-24 09:17:01 +0300

    created a check for ProductCategory

--- src/main/java/com/netcrackerg4/marketplace/constants/ValidationConstants.java
+++ src/main/java/com/netcrackerg4/marketplace/constants/ValidationConstants.java
@@ -5,4 +5,5 @@ public class ValidationConstants {
     public static final String PASSWORD_PATTERN =
             "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$";
     public static final String PHONE_PATTERN = "^[0-9]{10}$";
+    public static final String DATE_PATTERN = "yyyy-MM-dd";
 }
