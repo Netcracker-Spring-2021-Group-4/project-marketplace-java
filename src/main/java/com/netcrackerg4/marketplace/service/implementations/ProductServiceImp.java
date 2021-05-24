b6932502a18e90118f5eb57a4faba5150b0cf05e
Ivan Mordas <ivanmordas49@gmail.com>
2021-05-24 15:07:44 +0300

    add product

---
+++ src/main/java/com/netcrackerg4/marketplace/service/implementations/ProductServiceImp.java
@@ -0,0 +1,34 @@
+package com.netcrackerg4.marketplace.service.implementations;
+
+import com.netcrackerg4.marketplace.model.domain.AppProductEntity;
+import com.netcrackerg4.marketplace.model.dto.product.NewProductDto;
+import com.netcrackerg4.marketplace.repository.interfaces.IProductDao;
+import com.netcrackerg4.marketplace.service.interfaces.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImp implements IProductService {
+    private IProductDao productDao;
+
+    @Transactional
+    @Override
+    public void addProduct(String url, NewProductDto newProduct) {
+
+        AppProductEntity productEntity = AppProductEntity.builder()
+                .name(newProduct.getProductName())
+                .description(newProduct.getDescription())
+                .imageUrl(url)
+                .price(newProduct.getPrice())
+                .inStock(newProduct.getInStock())
+                .reserved(newProduct.getReserved())
+                .availabilityDate(newProduct.getDate())
+                .isActive(Boolean.TRUE)
+                .category(newProduct.getProductCategory())
+                .build();
+        productDao.createProduct(productEntity);
+
+    }
+}
