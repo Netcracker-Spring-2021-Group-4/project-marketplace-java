e8324c0d2e5ab0e39f028814b687dff9e1cc122e
Ivan Mordas <ivanmordas49@gmail.com>
2021-05-24 09:11:49 +0300

    create manager controller with add-product function

---
+++ src/main/java/com/netcrackerg4/marketplace/controller/ManagerController.java
@@ -0,0 +1,36 @@
+package com.netcrackerg4.marketplace.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netcrackerg4.marketplace.model.dto.user.NewProductDto;
+import com.netcrackerg4.marketplace.service.interfaces.IS3Service;
+import com.netcrackerg4.marketplace.service.interfaces.IUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URL;
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("/api/v1/manager")
+@AllArgsConstructor
+public class ManagerController {
+    private final IS3Service s3Service;
+    private final IUserService userService;
+
+    @PostMapping("/add-product")
+    void createProduct(@RequestParam(value = "file") MultipartFile multipartFile,
+                    @RequestParam(value = "product")  String product) throws JsonProcessingException {
+
+        UUID id = UUID.randomUUID();
+
+        ObjectMapper mapper = new ObjectMapper();
+        NewProductDto newProductDto = mapper.readValue(product, NewProductDto.class);
+
+        URL url = s3Service.uploadImage(id, multipartFile);
+
+        userService.addProduct(url.toString(),newProductDto);
+    }
+}
