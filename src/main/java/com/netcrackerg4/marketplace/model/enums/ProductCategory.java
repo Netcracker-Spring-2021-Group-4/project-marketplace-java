86a8ae8163c4eaf597f31e00e10111388fe54251
Ivan Mordas <ivanmordas49@gmail.com>
2021-05-24 09:22:49 +0300

    add all category for product

---
+++ src/main/java/com/netcrackerg4/marketplace/model/enums/ProductCategory.java
@@ -0,0 +1,17 @@
+package com.netcrackerg4.marketplace.model.enums;
+
+
+public enum ProductCategory {
+    RED_WINE,
+    ROSE_WINE,
+    WHITE_WINE,
+    DESSERT_WINE,
+    BLUE_CHEESE,
+    HARD_CHEESE,
+    PASTA_FILATA_CHEESE,
+    PROCESSED_CHEESE,
+    SEMI_HARD_CHEESE,
+    SEMI_SOFT_CHEESE,
+    SOFT_FRESH_CHEESE,
+    SOFT_RIPENED_CHEESE
+}
