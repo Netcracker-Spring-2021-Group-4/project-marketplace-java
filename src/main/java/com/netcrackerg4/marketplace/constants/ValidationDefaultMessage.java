2fca38d759ef9ce708ed4fc2ab23eec95db26672
Ivan Mordas <ivanmordas49@gmail.com>
2021-05-28 21:47:34 +0300

    fixed bug

--- src/main/java/com/netcrackerg4/marketplace/constants/ValidationDefaultMessage.java
+++ src/main/java/com/netcrackerg4/marketplace/constants/ValidationDefaultMessage.java
@@ -5,9 +5,9 @@ public class ValidationDefaultMessage {
     public static final String WRONG_FORMAT_PASSWORD =
             "Password has to contain at least one capital letter, lower case letter and number";
     public static final String WRONG_FORMAT_LAST_NAME =
-            "The first name must be of size between 2 and 30";
-    public static final String WRONG_FORMAT_FIRST_NAME =
             "The last name must be of size between 2 and 30";
+    public static final String WRONG_FORMAT_FIRST_NAME =
+            "The first name must be of size between 2 and 30";
     public static final String WRONG_FORMAT_PHONE_NUMBER =
             "The phone number must contain from 12 to 16 digits only.";
 }
