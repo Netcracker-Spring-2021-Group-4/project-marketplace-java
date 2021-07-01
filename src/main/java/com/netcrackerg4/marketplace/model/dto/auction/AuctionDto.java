74e48c3aa75da57adfbd53278d9b1681607386ec
Fillip Molodtsov <greatdrummer1@gmail.com>
2021-07-01 09:47:39 +0300

    using timestamp when creating auctions

--- src/main/java/com/netcrackerg4/marketplace/model/dto/auction/AuctionDto.java
+++ src/main/java/com/netcrackerg4/marketplace/model/dto/auction/AuctionDto.java
@@ -11,14 +11,14 @@ import javax.validation.constraints.Future;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 public class AuctionDto {
     @Future(message = ValidationDefaultMessage.WRONG_FORMAT_AUCTION_STARTS_AT)
-    private LocalDateTime startsAt;
+    private Timestamp startsAt;
     @Min(value= 100_00, message = ValidationDefaultMessage.WRONG_FORMAT_AUCTION_START_PRICE)
     @Max(value = 120_567_00, message = ValidationDefaultMessage.WRONG_FORMAT_AUCTION_START_PRICE_MAX)
     private int startPrice;
